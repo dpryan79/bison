dbd19040f1eb0eddd33a4f5c2be20d339ad179fb
dpryan79 <dpryan@dpryan.com>
2014-03-13 11:45:24 +0100

    Largely rewrote markduplicates. It now incorporates methylation calls in its decision making

--- markduplicates.c
+++ markduplicates.c
@@ -4,11 +4,34 @@
 
 #define WORD_OFFSET(b) b/32
 #define BIT_OFFSET(b) b%32
+#define GET_STRAND(b) b >> 30
+#define MIN_PHRED 5 //Minimum phred score that a basecall must have to be included in the methylation call bitmap
+
+//Lookup table for popcount, 16 bits so 4 function calls/value
+unsigned bitcounts_table[65536];
+
+//The following is only used for filling in the table above
+const uint64_t m1  = 0x5555555555555555; //binary: 0101...
+const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
+const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
+
+unsigned popcount(uint64_t x) {
+    x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+    x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits 
+    x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits 
+    return (x * h01)>>56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ... 
+}
+
+void fill_table() {
+    uint64_t i;
+    for(i=0;i<65536;i++) bitcounts_table[i] = popcount(i);
+}
 
 typedef struct {
-    int32_t tid, start1, start2, stop1, stop2;
-    int strand, MAPQ;
-    unsigned read_number;
+    int32_t tid, start1, start2, total_phred; //First 2 bits of tid denote strand: OT 00, OB 01, CTOT 10, CTOB 11, so ~1 billion contigs
+    uint64_t meth[2], unmeth[2];
+    uint32_t read_number;
 } alignment;
 
 typedef struct {
@@ -17,75 +40,83 @@ typedef struct {
     alignment *alignments;
 } qsort_func_struct;
 
+//Get the total phred score
+inline int32_t total_phred(bam1_t *read) {
+    int32_t l = read->core.l_qseq;
+    int32_t i=0, total=0;
+    uint8_t *qual = bam1_qual(read);
+
+    for(i=0; i<l; i++) {
+        total += *(qual+i);
+    }
+    return total;
+}
+
+//Get the total number of bits set in a uint64_t
+int lookup_popcount(uint64_t c) {
+    int total, i;
+
+    i = c & 0x000000000000FFFF;
+    total = bitcounts_table[i];
+    i = (c & 0x00000000FFFF0000) >> 16;
+    total += bitcounts_table[i];
+    i = (c & 0x0000FFFF00000000) >> 48;
+    total += bitcounts_table[i];
+    i = (c & 0xFFFF000000000000) >> 48;
+    total += bitcounts_table[i];
+    return total;
+}
+
 /*
     Sort a list of alignments, they'll be ordered as follows (always low to high):
-    (1) tid (chromosome index ID)
+    (1) tid (chromosome index ID), which is also strand
     (2) start1 (5' position of read #1)
-    (3) stop1 (3' position of read #1)
-    (4) start2 (5' position of read #2)
-    (5) stop2 (3' position of read #2)
-    (6) strand (0 OT, 1 OB, 2 CTOT, 3 CTOB)
-    (7) MAPQ (high to low)
+    (3) start2 (5' position of read #2)
+    (4) strand (0 OT, 1 OB, 2 CTOT, 3 CTOB)
+    (5) total calls, aka Hamming weight (high to low)
+    (6) total phred (high to low)
 */
 int comp_func(const void *a, const void *b) {
     alignment *a1 = (alignment*) a;
     alignment *a2 = (alignment*) b;
+    int ncalls1, ncalls2;
 
-    if(a1->tid < a2->tid) return -1;
+    if(a1->tid < a2->tid) return -1; //tid, strand
     else if(a1->tid > a2->tid) return 1;
     else {
-        if(a1->start1 < a2->start1) return -1;
+        if(a1->start1 < a2->start1) return -1; //start1
         else if(a1->start1 > a2->start1) return 1;
         else {
-            if(a1->stop1 < a2->stop1) return -1;
-            else if(a1->stop1 > a2->stop1) return 1;
+            if(a1->start2 < a2->start2) return -1; //start2
+            else if(a1->start2 > a2->start2) return 1;
             else {
-                if(a1->start2 < a2->start2) return -1;
-                else if(a1->start2 > a2->start2) return 1;
+                ncalls1 = lookup_popcount(a1->meth[0]) + lookup_popcount(a1->meth[1]) + lookup_popcount(a1->unmeth[0]) + lookup_popcount(a1->unmeth[1]);
+                ncalls2 = lookup_popcount(a2->meth[0]) + lookup_popcount(a2->meth[1]) + lookup_popcount(a2->unmeth[0]) + lookup_popcount(a2->unmeth[1]);
+                //ncalls1 = popcount(a1->meth[0])+popcount(a1->meth[1])+popcount(a1->unmeth[0])+popcount(a1->unmeth[1])
+                //ncalls2 = popcount(a2->meth[0])+popcount(a2->meth[1])+popcount(a2->unmeth[0])+popcount(a2->unmeth[1])
+                if(ncalls1 > ncalls2) return -1; //total calls
+                else if(ncalls1 < ncalls2) return 1;
                 else {
-                    if(a1->stop2 < a2->stop2) return -1;
-                    else if(a1->stop2 > a2->stop2) return 1;
-                    else {
-                        if(a1->strand < a2->strand) return -1;
-                        else if(a1->strand > a2->strand) return 1;
-                        else { //This will be the other way around
-                            if(a1->MAPQ > a2->MAPQ) return -1; 
-                            else if(a1->MAPQ < a2->MAPQ) return 1; 
-                            else return 0;
-                        }
-                    }
+                    if(a1->total_phred > a2->total_phred) return -1; //sum of phred scores, this is backwords
+                    else if(a1->total_phred < a2->total_phred) return 1;
+                    else return 0;
                 }
             }
         }
     }
 }
 
-//This is the same as comp_func(), except that MAPQ is ignored
-int comp_func2(const void *a, const void *b) {
-    alignment *a1 = (alignment*) a;
-    alignment *a2 = (alignment*) b;
-
+//This is the same as comp_func(), with only tid, start1, start2 and strand used
+int comp_func2(alignment *a1, alignment *a2) {
     if(a1->tid < a2->tid) return -1;
     else if(a1->tid > a2->tid) return 1;
     else {
         if(a1->start1 < a2->start1) return -1;
         else if(a1->start1 > a2->start1) return 1;
         else {
-            if(a1->stop1 < a2->stop1) return -1;
-            else if(a1->stop1 > a2->stop1) return 1;
-            else {
-                if(a1->start2 < a2->start2) return -1;
-                else if(a1->start2 > a2->start2) return 1;
-                else {
-                    if(a1->stop2 < a2->stop2) return -1;
-                    else if(a1->stop2 > a2->stop2) return 1;
-                    else {
-                        if(a1->strand < a2->strand) return -1;
-                        else if(a1->strand > a2->strand) return 1;
-                        else return 0;
-                    }
-                }
-            }
+            if(a1->start2 < a2->start2) return -1;
+            else if(a1->start2 > a2->start2) return 1;
+            else return 0;
         }
     }
 }
@@ -115,32 +146,95 @@ int get_bit(uint32_t *map, uint64_t n) {
     return bit != 0; 
 }
 
-uint64_t mark_dups(alignment *alignments, uint32_t *bitmap, uint64_t total_pairs) {
-    uint64_t i, ndups = 0;
-    void *cur_alignment = (void *) alignments;
+//Return the number of reads to compare for PCR duplicates, based on tid,
+//start1, and start2
+uint64_t get_bin_limits(alignment *alignments, uint64_t total) {
+    uint64_t i, n = 1;
+    alignment *next_alignment;
 
-    for(i=1;i<total_pairs;i++) {
-        if(comp_func2(cur_alignment, (void *) (alignments+i)) == 0) {
-            set_bit(bitmap, alignments[i].read_number);
-            ndups++;
-        } else {
-            cur_alignment = (void *) (alignments+i);
+    for(i=1; i<total; i++) {
+        next_alignment = alignments+i;
+        if(comp_func2(alignments, next_alignment) != 0) break;
+        n++;
+    }
+    return n;
+}
+
+//Return the edit distance between two methylation call string. The edit distance is unaffected by call/no-call differences in the same position
+/*
+    The general idea is:
+    (1) XOR the METH calls
+    (2) XOR the UNMETH calls
+    (3) AND the results
+    (4) the popcount of the result is the edit distance
+*/
+int edit_distance(alignment *a1, alignment *a2) {
+    int distance = 0;
+    uint64_t diff1, diff2;
+
+    //read #1
+    diff1 = a1->meth[0] ^ a2->meth[0];
+    diff2 = a1->unmeth[0] ^ a2->unmeth[0];
+    distance += lookup_popcount(diff1 & diff2);
+
+    //read #2
+    diff1 = a1->meth[1] ^ a2->meth[1];
+    diff2 = a1->unmeth[1] ^ a2->unmeth[1];
+    distance += lookup_popcount(diff1 & diff2);
+
+    return distance;
+}
+
+//Mark the duplicates in a bin
+uint64_t mark_bin(alignment *alignments, uint64_t i, uint64_t bin_width, uint32_t *bitmap) {
+    uint64_t j, k, ndups=0;
+
+    for(j=i; j<i+bin_width-1; j++) { //There's no point in starting on the last one
+        //If this read is already a duplicate, then ignore
+        if(get_bit(bitmap, j) == 0) {
+            for(k=j+1; k<j+bin_width; k++) {
+                if(get_bit(bitmap, k) == 0) {
+                    if(edit_distance(alignments+j, alignments+k) > 0) {
+                        set_bit(bitmap, k);
+                        ndups++;
+                    }
+                }
+            }
         }
     }
     return ndups;
 }
-        
+    
+uint64_t mark_dups(alignment *alignments, uint32_t *bitmap, uint64_t total_pairs) {
+    uint64_t i = 0, ndups = 0, bin_width;
+
+    while(i<total_pairs) {
+        /*
+            Process the alignments in bins of possible duplicates (by tid, start1, start2)
+            Within each bin, mark duplicates.
+        */
+        bin_width = get_bin_limits(alignments+i, total_pairs-i);
+        if(bin_width > 1) {
+            ndups += mark_bin(alignments, i, bin_width, bitmap);
+        }
+        i+=bin_width;
+    }
+    return ndups;
+}
 
 void usage(char *prog) {
     printf("Usage: %s [OPTIONS] input.bam output.bam\n", prog);
     printf("\n\
 This program will parse a BAM file produced by bison and mark likely PCR\n\
 duplicates in a new file. A PCR duplicate is defined as two reads/read pairs\n\
-having the same start and stop coordinates on the some strand of the same\n\
-chromosome. The read or pair with the best MAPQ score will be kept.\n\
+having the same start coordinates on the same strand of the same\n\
+chromosome/contig and having a methylation call edit-distance of 0 (ignoring\n\
+instances of a no-call in one read and a call in the other). The read or pair\n\
+with the greater sum of base-call phred scores is left unmarked. Note that\n\
+soft-clipped bases are included in computing the 5' coordinate of the read,\n\
+as is also the case with picard markDuplicates.\n\
 \n\
-There are better ways to go about this (both in terms of algorithms and in the\n\
-information used to determine a PCR duplicate), but this will normally suffice.\n\
+N.B., only methylation calls with a phred score of at least 5 are compared.\n\
 \n\
 N.B., this program does not currently support coordinate-sorted BAM files or\n\
 paired-end files containing discordant or other mixed alignments where the reads\n\
@@ -156,7 +250,7 @@ are on different chromosomes or different strand.\n");
            it's full). The default is 1000000.\n");
 }
 
-inline int get_strand(bam1_t *read) {
+inline int32_t get_strand(bam1_t *read) {
     char *XG = bam_aux2Z(bam_aux_get(read, "XG"));
     char *XR = bam_aux2Z(bam_aux_get(read, "XR"));
 
@@ -166,6 +260,60 @@ inline int get_strand(bam1_t *read) {
     } else {
         if(*XR == 'C') return 1; //OB
         else return 3; //CTOB
+    }
+}
+
+inline int32_t get_pos(bam1_t *read) {
+    int32_t pos = read->core.pos;
+    uint32_t *CIGAR = bam1_cigar(read);
+    int32_t op, nops;
+
+    if(read->core.n_cigar == 1) return pos;
+
+    op = CIGAR[0] & 0xf;
+    nops = CIGAR[0] >> 4;
+    if(op == BAM_CHARD_CLIP) { //The only thing that can precede an S is an H
+        op = CIGAR[1] & 0xf;
+        nops = CIGAR[1] >> 4;
+        if(op == BAM_CSOFT_CLIP) pos -= nops;
+    } else if(op == BAM_CSOFT_CLIP) {
+        pos -= nops;
+    }
+    if(pos<0) pos=0;
+    return pos;
+}
+
+//return the tid, such that first 2 bits denote the strand
+inline int32_t get_tid(bam1_t *read) {
+    int32_t strand = get_strand(read);
+
+    strand = strand << 30;
+    assert(read->core.tid < 0x3FFFFFFF);
+    return strand & read->core.tid;
+}
+
+//Given an XM string, set two 64bit bit-fields denoting methylated/unmethylated positions
+//Only include things if a call has a phred score of at least MIN_PHRED
+void calls2uint(bam1_t *read, uint64_t *meth, uint64_t *unmeth) {
+    int32_t i, l = read->core.l_qseq;
+    uint8_t *p = bam_aux_get(read, "XM");
+    char *XM = NULL;
+    uint8_t *qual = bam1_qual(read);
+
+    *meth = 0;
+    *unmeth = 0;
+    if(p != NULL) {
+        XM = bam_aux2Z(p);
+        for(i=0; i<l; i++) {
+            switch(*(XM+i)) {
+                case 'Z' : case 'X' : case 'H' :
+                    if(*(qual+i) >= MIN_PHRED) meth[WORD_OFFSET(i)] |= (1 << BIT_OFFSET(i)); //This is just set_bit() with a 64bit map
+                    break;
+                case 'z' : case 'x' : case 'h' :
+                    if(*(qual+i) >= MIN_PHRED) unmeth[WORD_OFFSET(i)] |= (1 << BIT_OFFSET(i));
+                    break;
+            }
+        }
     }
 }
 
@@ -218,20 +366,26 @@ int main(int argc, char *argv[]) {
     alignments = malloc(sizeof(alignment)*max_length);
     assert(alignments != NULL);
 
+    //Initialize lookup table
+    fill_table();
+
     //Read in the alignments
     while(bam_read1(fp, read) > 1) {
-        alignments[total_pairs].tid = read->core.tid;
-        alignments[total_pairs].start1 = read->core.pos;
-        alignments[total_pairs].stop1 = bam_calend(&(read->core), bam1_cigar(read));
-        alignments[total_pairs].MAPQ = read->core.qual;
-        alignments[total_pairs].strand = get_strand(read);
+        if(read->core.flag & BAM_FUNMAP) continue; //We just skip over unmapped reads
+
+        alignments[total_pairs].tid = get_tid(read);
+        alignments[total_pairs].start1 = get_pos(read);
+        alignments[total_pairs].total_phred = total_phred(read);
+        calls2uint(read, &(alignments[total_pairs].meth[0]), &(alignments[total_pairs].unmeth[0]));
         if(read->core.flag & BAM_FPAIRED) {
             assert(bam_read1(fp,read)>1);
-            alignments[total_pairs].start2 = read->core.pos;
-            alignments[total_pairs].stop2 = bam_calend(&(read->core), bam1_cigar(read));
+            alignments[total_pairs].start2 = get_pos(read);
+            alignments[total_pairs].total_phred += total_phred(read);
+            calls2uint(read, &(alignments[total_pairs].meth[1]), &(alignments[total_pairs].unmeth[1]));
         } else {
             alignments[total_pairs].start2 = 0;
-            alignments[total_pairs].stop2 = 0;
+            alignments[total_pairs].meth[1] = 0;
+            alignments[total_pairs].unmeth[1] = 0;
         }
         alignments[total_pairs].read_number = total_pairs;
         total_pairs++;
@@ -266,14 +420,15 @@ int main(int argc, char *argv[]) {
     bam_header_write(of, header);
 
     while(bam_read1(fp, read) > 1) {
-        if(get_bit(bitmap, cur_read)) read->core.flag = read->core.flag | BAM_FDUP;
+        //Only set as duplicate if it's mapped
+        if(!(read->core.flag & BAM_FUNMAP)) if(get_bit(bitmap, cur_read)) read->core.flag = read->core.flag | BAM_FDUP;
         bam_write1(of, read);
         if(read->core.flag & BAM_FPAIRED) {
             assert(bam_read1(fp, read) > 1);
-            if(get_bit(bitmap, cur_read)) read->core.flag = read->core.flag | BAM_FDUP;
+            if(!(read->core.flag & BAM_FUNMAP)) if(get_bit(bitmap, cur_read)) read->core.flag = read->core.flag | BAM_FDUP;
             bam_write1(of, read);
         }
-        cur_read++;
+        if(!(read->core.flag & BAM_FUNMAP)) cur_read++;
     }
 
     //Clean up
