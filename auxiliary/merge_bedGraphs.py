d985e43d387180c8bdedbc2bd25bc1e803231200
dpryan79 <dpryan@dpryan.com>
2014-03-17 17:10:18 +0100

    fix a bug in merge_bedgraphs that was triggered due to adding a track line

--- auxiliary/merge_bedGraphs.py
+++ auxiliary/merge_bedGraphs.py
@@ -22,7 +22,7 @@ of.write("track type=bedGraph\n")
 lines = []
 for f in files :
     line = f.next()
-    if(line.startswith('track')) :
+    if(line[0].startswith('track')) :
         line = f.next()
     lines.append([line[0],int(line[1]), int(line[2]), int(line[4]), int(line[5])])
 
