921b5ec46942429c41b976f7e09f8d03d8ed415a
dpryan79 <dpryan@dpryan.com>
2014-09-02 17:55:04 +0200

    Remove debugging printf

--- master.c
+++ master.c
@@ -1175,7 +1175,7 @@ void * master_processer_thread(void *a) {
         } else {
             best_node = process_paired(node1_read, node2_read, node3_read, node4_read, seq); //Output is stored in read
         }
-        printf("%"PRId32"\n", best_node); fflush(stdout);
+
         best_read1 = NULL;
         best_read2 = NULL;
         if(best_node & 0x1) { //OT
